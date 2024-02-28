01c99b5002fcce537d040180f84d0791f6ce11f6
ViniCarmo <vinicius.carmo1106@gmail.com>
2024-02-28 16:43:32 -0300

    post, delete, get

--- src/main/java/Application/Programa.java
+++ src/main/java/Application/Programa.java
@@ -2,14 +2,38 @@ package Application;
 
 import Dominio.Pessoa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+
 public class Programa {
     public static void main(String[] args) {
 
-        Pessoa p1 = new Pessoa(1, "Joao Paulo", "joaopaulo@gmail.com");
-        Pessoa p2 = new Pessoa(2, "Paulo Jesus", "PauloJesus@gmail.com");
-        Pessoa p3 = new Pessoa(3, "Marcela Almeida", "marcelaalmeida@gmail.com");
+        Pessoa p1 = new Pessoa(null, "Joao Paulo", "joaopaulo@gmail.com");
+        Pessoa p2 = new Pessoa(null, "Paulo Jesus", "PauloJesus@gmail.com");
+        Pessoa p3 = new Pessoa(null, "Marcela Almeida", "marcelaalmeida@gmail.com");
 
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("exemplo-jpa");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
 
+        /* add no banco
+        entityManager.getTransaction().begin();
+        entityManager.persist(p1);
+        entityManager.persist(p2);
+        entityManager.persist(p3);
+        entityManager.getTransaction().commit();
+         */
 
+        /* Imprimindo cadstro do banco
+        Pessoa p = entityManager.find(Pessoa.class,2);
+        System.out.println(p);
+         */
+
+        entityManager.find(Pessoa.class, 2);
+        entityManager.getTransaction().begin();
+        entityManager.remove(2);
+        entityManager.getTransaction().commit();
+        System.out.println("Pronto");
     }
 }
--- src/main/java/Dominio/Pessoa.java
+++ src/main/java/Dominio/Pessoa.java
@@ -1,10 +1,19 @@
 package Dominio;
 
+import jdk.jfr.Name;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
+@Entity
 public class Pessoa implements Serializable {
 
     private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     public Integer id;
     public String nome;
     public String email;
