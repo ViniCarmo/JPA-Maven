1201f423f7fd790579effcece5733fd6ef5806e6
ViniCarmo <vinicius.carmo1106@gmail.com>
2024-02-28 15:27:53 -0300

    initial commit

---
+++ src/main/java/Application/Programa.java
@@ -0,0 +1,15 @@
+package Application;
+
+import Dominio.Pessoa;
+
+public class Programa {
+    public static void main(String[] args) {
+
+        Pessoa p1 = new Pessoa(1, "Joao Paulo", "joaopaulo@gmail.com");
+        Pessoa p2 = new Pessoa(2, "Paulo Jesus", "PauloJesus@gmail.com");
+        Pessoa p3 = new Pessoa(3, "Marcela Almeida", "marcelaalmeida@gmail.com");
+
+
+
+    }
+}
